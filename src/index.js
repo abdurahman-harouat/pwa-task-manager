e884fdaa2d8ed2b2e67af32a852107f08db510ac
abdurahman-harouat <abderrahmane.harouat@gmail.com>
2025-11-28 14:53:21 +0100

    registering service worker

--- src/index.js
+++ src/index.js
@@ -9,3 +9,13 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// Register service worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(() => console.log("Service Worker registered"))
+      .catch((error) => console.log("Service Worker registration failed:", error));
+  });
+}
